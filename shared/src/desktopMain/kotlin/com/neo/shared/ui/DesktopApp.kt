dd6f72b05ef554008afa97030b4f068e18427cd9
irineu silva <mmmirineusilva@gmail.com>
2023-10-24 23:22:13 -0300

    Improve desktop file picker

--- shared/src/desktopMain/kotlin/com/neo/shared/ui/DesktopApp.kt
+++ shared/src/desktopMain/kotlin/com/neo/shared/ui/DesktopApp.kt
@@ -5,7 +5,6 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import com.darkrockstudios.libraries.mpfilepicker.FilePicker
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.io.File
 
@@ -18,7 +17,14 @@ fun DesktopApp() {
 
     var content by remember { mutableStateOf<String?>(null) }
 
-    val coroutine = rememberCoroutineScope()
+    LaunchedEffect(file) {
+
+        content = null
+
+        content = withContext(Dispatchers.Default) {
+            file?.readText()
+        }
+    }
 
     App(
         openFilePicker = {
@@ -37,15 +43,7 @@ fun DesktopApp() {
         showFilePicker = false
 
         if (selectedFile != null) {
-
             file = selectedFile.platformFile as File
-            content = null
-
-            coroutine.launch {
-                content = withContext(Dispatchers.IO) {
-                    file?.readText()
-                }
-            }
         }
     }
 }
