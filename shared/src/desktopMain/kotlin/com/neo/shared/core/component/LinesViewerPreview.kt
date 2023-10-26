0512202178101aa2eb48650fdae88350ce24696d
irineu silva <mmmirineusilva@gmail.com>
2023-10-26 00:53:01 -0300

    Change lines viewer preview

--- shared/src/desktopMain/kotlin/com/neo/shared/core/component/LinesViewerPreview.kt
+++ shared/src/desktopMain/kotlin/com/neo/shared/core/component/LinesViewerPreview.kt
@@ -1,9 +1,12 @@
 package com.neo.shared.core.component
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.neo.shared.core.model.Line
@@ -13,19 +16,23 @@ import com.neo.shared.core.util.createArray
 @Composable
 fun LinesViewerPreview() {
 
-    val lines = createArray("file") {
+    val lines = createArray("users") {
         addObject {
             addLiteral("name", "neu")
             addLiteral("dev", true)
+            addArray("skills", collapsed = true) {}
         }
 
-        addObject(collapsed = true) {
+        addObject {
             addLiteral("name", "vini")
             addLiteral("dev", true)
+            addArray("skills", collapsed = true) {}
         }
     }
 
     MaterialTheme {
-        LinesViewer(lines, Modifier.padding(16.dp))
+       Box(Modifier.fillMaxSize(), Alignment.Center) {
+           LinesViewer(lines, Modifier.padding(16.dp))
+       }
     }
 }
